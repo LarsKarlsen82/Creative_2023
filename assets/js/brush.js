b2472e263ee26d424a7a8932aef8a9f58996367d
LarsKarlsen82 <lpuk82@gmail.com>
2023-12-20 14:43:25 +0100

    updateupdate

--- assets/js/brush.js
+++ assets/js/brush.js
@@ -13,22 +13,29 @@ const colours = [
 
 const maxSize = 40;
 const minSize = 0;
-const mouseRadius = 60;
+const touchRadius = 60;
 
-//mouse position
-let mouse = {
+// mouse and touch position
+let pointer = {
     x: null,
     y: null
+};
+
+// function to handle both mouse and touch events
+function handleMove(event) {
+    if (event.touches) {
+        pointer.x = event.touches[0].clientX;
+        pointer.y = event.touches[0].clientY;
+    } else {
+        pointer.x = event.clientX;
+        pointer.y = event.clientY;
+    }
 }
 
-window.addEventListener('mousemove', function (event) {
-        mouse.x = event.x;
-        mouse.y = event.y;
-        console.log(mouse);
-    }
-)
+window.addEventListener('mousemove', handleMove);
+window.addEventListener('touchmove', handleMove, { passive: false });
 
-//create constructor function for particle
+// create constructor function for particle
 function Particle(x, y, directionX, directionY, size, colour) {
     this.x = x;
     this.y = y;
@@ -38,7 +45,7 @@ function Particle(x, y, directionX, directionY, size, colour) {
     this.colour = colour;
 }
 
-//add draw method to particle prototype
+// add draw method to particle prototype
 Particle.prototype.draw = function () {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
@@ -46,30 +53,28 @@ Particle.prototype.draw = function () {
     ctx.fill();
 }
 
-//add update method to particle prototype
-Particle.prototype.update = function(){
-    if (this.x + this.size*2 > canvas.width || this.x - this.size*2 < 0){
-			this.directionX = -this.directionX;
-	} if (this.y + this.size*2 > canvas.height || this.y - this.size*2 < 0){
-		this.directionY = -this.directionY;
-	}
-    this.x +=this.directionX;
+// add update method to particle prototype
+Particle.prototype.update = function () {
+    if (this.x + this.size * 2 > canvas.width || this.x - this.size * 2 < 0) {
+        this.directionX = -this.directionX;
+    }
+    if (this.y + this.size * 2 > canvas.height || this.y - this.size * 2 < 0) {
+        this.directionY = -this.directionY;
+    }
+    this.x += this.directionX;
     this.y += this.directionY;
 
-
-    // mouse interactivity
-    let mouseRadius = 50;
-    if (mouse.x - this.x < mouseRadius 
-    && mouse.x - this.x >-mouseRadius 
-    && mouse.y - this.y < mouseRadius 
-    && mouse.y - this.y >-mouseRadius) {
-	    if (this.size < maxSize){
-		    this.size += 3;
-            this.x -=1.5;
-	    } 
-    }
-    else if (this.size > minSize){
-		    this.size -= .1;
+    // pointer interactivity (both mouse and touch)
+    if (pointer.x - this.x < touchRadius &&
+        pointer.x - this.x > -touchRadius &&
+        pointer.y - this.y < touchRadius &&
+        pointer.y - this.y > -touchRadius) {
+        if (this.size < maxSize) {
+            this.size += 3;
+            this.x -= 1.5;
+        }
+    } else if (this.size > minSize) {
+        this.size -= 0.1;
     }
     if (this.size < 0) {
         this.size = 0;
@@ -77,28 +82,28 @@ Particle.prototype.update = function(){
     this.draw();
 }
 
-//create particle array
+// create particle array
 function init() {
     particleArray = [];
-    for (let i=0; i < 1000; i++) {
+    for (let i = 0; i < 1000; i++) {
         let size = 10;
         let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2);
         let y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2);
-        let directionX = (Math.random() * .2) - .1;
-        let directionY = (Math.random() * .2) - .1;
+        let directionX = (Math.random() * 0.2) - 0.1;
+        let directionY = (Math.random() * 0.2) - 0.1;
         let colour = colours[Math.floor(Math.random() * colours.length)];
 
         particleArray.push(new Particle(x, y, directionX, directionY, size, colour));
     }
-  console.log(particleArray);
+    console.log(particleArray);
 }
 
-//animation loop
+// animation loop
 function animate() {
     requestAnimationFrame(animate);
-    ctx.clearRect(0 , 0, innerWidth, innerHeight);
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
 
-    for (let i = 0; i < particleArray.length; i ++) {
+    for (let i = 0; i < particleArray.length; i++) {
         particleArray[i].update();
     }
 }
@@ -110,11 +115,10 @@ window.addEventListener('resize', () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     init();
+});
 
-})
-
-//remove mouse position periodically
+// remove pointer position periodically
 setInterval(function () {
-    mouse.x = undefined;
-    mouse.y = undefined;
+    pointer.x = undefined;
+    pointer.y = undefined;
 }, 1000);
